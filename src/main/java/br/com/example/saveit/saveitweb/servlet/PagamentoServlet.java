d160a9f70a3ec379197a1ff1f7e28424adc1b189
GustavoManganelli <gugu.manganeli@gmail.com>
2025-10-29 18:38:05 -0300

    Criação da classe PagamentoServlet e metodo bsucarDadosPlano da classe PagamentosDAO feito, mas a pagamentos está com erro na hora de enviar para a pagamentos.jsp

--- src/main/java/br/com/example/saveit/saveitweb/servlet/PagamentoServlet.java
+++ src/main/java/br/com/example/saveit/saveitweb/servlet/PagamentoServlet.java
@@ -1,7 +1,9 @@
 package br.com.example.saveit.saveitweb.servlet;
 
 import br.com.example.saveit.saveitweb.model.pagamento.Pagamento;
+import br.com.example.saveit.saveitweb.model.pagamento.PagamentoDAO;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,22 +11,59 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/pagamentoServlet")
 public class PagamentoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Pagamento pagamento = new Pagamento();
+        PagamentoDAO pagamentoDAO = new PagamentoDAO();
+        List<String> dadosPlano;
 
         HttpSession sessao = request.getSession();
         Object admin = request.getAttribute("admin");
 
         if (admin == null) {
             String nome = (String) sessao.getAttribute("nome");
+            int id = (int) sessao.getAttribute("id");
+            String url = (String) sessao.getAttribute("url");
+            String plano = (String) sessao.getAttribute("plano");
+            dadosPlano = pagamentoDAO.buscarDadosPlano(id);
+            String descricao = dadosPlano.get(0);
+            String preco =  dadosPlano.get(1);
+            String status = dadosPlano.get(2);
+            String idPlano = dadosPlano.get(3);
+            String dt_criacao = dadosPlano.get(4);
+            String dt_validade = dadosPlano.get(5);
+            String forma_pagamento = dadosPlano.get(6);
+
+
+
+            try{
+                sessao.setAttribute("descricao", descricao);
+                sessao.setAttribute("preco", preco);
+                sessao.setAttribute("status", status);
+                sessao.setAttribute("idPlano", idPlano);
+                sessao.setAttribute("dt_criacao", dt_criacao);
+                sessao.setAttribute("dt_validade", dt_validade);
+                sessao.setAttribute("forma_pagamento", forma_pagamento);
+                sessao.setAttribute("nome", nome);
+                sessao.setAttribute("id", id);
+                sessao.setAttribute("url", url);
+                sessao.setAttribute("plano", plano);
+                sessao.setAttribute("dadosPlano", dadosPlano);
+
+                request.getRequestDispatcher("/WEB-INF/view/admin/pagamentos.jsp").forward((ServletRequest) sessao, response);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
         }
 
 
-        request.getRequestDispatcher("/WEB-INF/view/admin/pagamentos.jsp").forward(request, response);
+
+
+
     }
 
 }
